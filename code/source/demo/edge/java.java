8ebf5cb861afaac1fcdc281b175682e493f1032e
IrfanaSabreen <Sabreen13Irfana@gmail.com>
2023-10-25 13:22:43 +0530

    DemoEdge1

---
+++ code/source/demo/edge/java.java
@@ -0,0 +1,38 @@
+package demo.edge;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class java
+
+{
+	// ---( internal utility methods )---
+
+	final static java _instance = new java();
+
+	static java _newInstance() { return new java(); }
+
+	static java _cast(Object o) { return (java)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void deleteAccount (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(deleteAccount)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
